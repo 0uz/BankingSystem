b33cbc1747781b232c645dc21e573eaba2480d1f
0uz <duymaz_14@hotmail.com>
2020-11-09 12:49:52 +0300

    login screen

--- src/com/LoginScreenController.java
+++ src/com/LoginScreenController.java
@@ -2,11 +2,22 @@ package com;
 
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class LoginScreenController {
     public Button exitButton;
     public Button minimizeButton;
+    public ImageView logo;
+
+    public void initialize(){
+        File file = new File("images/logo.png");
+        Image image = new Image(file.toURI().toString());
+        logo.setImage(image);
+    }
 
     public void exitButton(){
         System.exit(1);
