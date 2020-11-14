d2d59d1b2f6d7dff8be97a312de13718119c547a
0uz <duymaz_14@hotmail.com>
2020-11-15 01:02:27 +0300

    Last

---
+++ src/com/MainScreenController.java
@@ -0,0 +1,17 @@
+package com;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
+public class MainScreenController {
+
+    public ImageView buttonIcon;
+
+    public void initialize(){
+        File file = new File("images/phone.png");
+        Image image = new Image(file.toURI().toString());
+        buttonIcon.setImage(image);
+    }
+}
